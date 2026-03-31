622fc6648e4fb5cf1337776f4a7f50b71830b6aa
gesslar <1266935+gesslar@users.noreply.github.com>
2026-03-31 00:56:22 -0400

    mew

---
+++ doc/wiki/src/components/class-browser/index.ts
@@ -0,0 +1,7 @@
+export { default as ClassBrowser } from "./ClassBrowser.astro";
+export { default as ClassExtends } from "./ClassExtends.astro";
+export { default as ClassMethod } from "./ClassMethod.astro";
+export { default as ClassMethodInput } from "./ClassMethodInput.astro";
+export { default as ClassMethodOutput } from "./ClassMethodOutput.astro";
+export { default as ClassProperty } from "./ClassProperty.astro";
+export { default as ClassPropertyType } from "./ClassPropertyType.astro";
