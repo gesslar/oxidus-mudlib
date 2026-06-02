c66944b4109859fa5bc7424dfbe07b9cf7442965
gesslar <1266935+gesslar@users.noreply.github.com>
2026-06-02 16:45:19 -0400

    removing mail

--- include/objects.h
+++ include/objects.h
@@ -4,7 +4,6 @@
 #include <dirs.h>
 
 #define OBJ_LOOT                DIR_OBJ           "loot/loot"
-#define OBJ_MAIL_CLIENT         DIR_OBJ_MUDLIB    "mail_clients/mail_client"
 #define OBJ_NODE                DIR_OBJ_NODE      "resource_node"
 #define OBJ_SECURITY_EDITOR     DIR_ADM_OBJ       "security_editor"
 #define OBJ_SOUL_TOOL           DIR_OBJ_MUDLIB    "emote_admin/emote_client"
