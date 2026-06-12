ea55095805bf4c5b62f52c44fc3c79d728cb9fc1
gesslar <1266935+gesslar@users.noreply.github.com>
2026-06-12 00:59:22 -0400

    new stat include

---
+++ include/stat.h
@@ -0,0 +1,9 @@
+#ifndef __STAT_H__
+#define __STAT_H__
+
+#define STAT_FILE_SIZE 0
+#define STAT_LAST_MODIFIED 1
+#define STAT_TIME_OBJECT_LOADED 2
+#define STAT_TIME_FILE_CREATED 3
+
+#endif // __STAT_H__
