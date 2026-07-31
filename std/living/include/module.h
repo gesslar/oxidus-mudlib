5d35992c4a843dccbabf70bd1be4147a6abeb351
gesslar <1266935+gesslar@users.noreply.github.com>
2026-07-31 00:36:12 -0400

    x

--- std/living/include/module.h
+++
@@ -1,8 +0,0 @@
-#ifndef __LIVING_MODULE_H__
-#define __LIVING_MODULE_H__
-
-// Module management is now provided at the object level.
-// See /std/object/module.c and /include/module.h
-#include <module.h>
-
-#endif // __LIVING_MODULE_H__
