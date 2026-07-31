9dfeb0b4d451b7c5c0d8df018d983a1c076825b6
gesslar <1266935+gesslar@users.noreply.github.com>
2026-07-30 23:50:45 -0400

    new damage functions

--- std/living/include/damage.h
+++ std/living/include/damage.h
@@ -4,6 +4,10 @@
 public float adjust_damage_by_severity(float damage, string severity);
 public varargs float calculate_damage(object enemy, mixed weapon_or_skill);
 public float deliver_damage(object victim, float damage, string type);
+public float deliver_magic_damage(object victim, float damage, string type);
+public float deliver_mundane_damage(object victim, float damage, string type);
 public float receive_damage(object attacker, float damage, string type);
+public float receive_magic_damage(object attacker, float damage, string type);
+public float receive_mundane_damage(object attacker, float damage, string type);
 
 #endif // __DAMAGE_H__
