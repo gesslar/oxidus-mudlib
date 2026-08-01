09c470073efafe354752addc96a5c153e8fb623b
gesslar <1266935+gesslar@users.noreply.github.com>
2026-08-01 12:41:26 -0400

    also docs

--- std/object/include/module.h
+++ std/object/include/module.h
@@ -1,11 +1,15 @@
 #ifndef __OBJECT_MODULE_H__
 #define __OBJECT_MODULE_H__
 
-varargs object add_module(string module_file, mixed args...);
-mixed query_module(string module_name);
-int remove_module(string module_name);
-mapping query_modules();
-varargs mixed module(string module_name, string function_name, mixed args...);
-void remove_all_modules();
+private void valid_module_name(string module_name, int index);
+private void valid_function_name(string function_name, int index);
+private void detach_and_destruct(object mod);
+
+public varargs object add_module(string module_file, mixed args...);
+public mixed query_module(string module_name);
+public int remove_module(string module_name);
+public mapping query_modules();
+public varargs mixed module(string module_name, string function_name, mixed args...);
+public void remove_all_modules();
 
 #endif // __OBJECT_MODULE_H__
