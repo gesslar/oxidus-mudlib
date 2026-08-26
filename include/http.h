fca930d918bc92350327c88b33376751e9bddcf8
gesslar <1266935+gesslar@users.noreply.github.com>
2026-08-26 19:37:04 -0400

    Add handling for HTTP 413 Content Too Large response in HTTP server

--- include/http.h
+++ include/http.h
@@ -21,6 +21,7 @@
 #define HTTP_STATUS_FORBIDDEN "403 Forbidden"
 #define HTTP_STATUS_NOT_FOUND "404 Not Found"
 #define HTTP_STATUS_METHOD_NOT_ALLOWED "405 Method Not Allowed"
+#define HTTP_STATUS_CONTENT_TOO_LARGE "413 Content Too Large"
 #define HTTP_STATUS_INTERNAL_SERVER_ERROR "500 Internal Server Error"
 #define HTTP_STATUS_NOT_IMPLEMENTED "501 Not Implemented"
 #define HTTP_STATUS_BAD_GATEWAY "502 Bad Gateway"
