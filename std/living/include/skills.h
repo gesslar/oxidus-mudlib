f222bbcc09d8fb9200d2d0e10ed063d9c3756b7f
gesslar <1266935+gesslar@users.noreply.github.com>
2026-08-31 20:47:04 -0400

    adjusting combat again

--- std/living/include/skills.h
+++ std/living/include/skills.h
@@ -13,14 +13,16 @@ int has_skill(string skill);
 int set_skill_level(string skill, float level);
 mapping query_skills();
 void set_skills(mapping s);
-varargs float improve_skill(string skill, mixed progress);
+varargs float improve_skill(string skill, mixed potential_progress);
 int query_skill_progress(string skill);
 int modify_skill_level(string skill, int level);
-int adjust_skills_by_npc_level(float level);
+public int adjust_skills_by_npc_level(float level);
 private nomask mapping find_skill_node(string skill);
 private nomask mapping adjust_skill_levels(mapping current_skills, float level);
 string *query_skill_path(string skill);
 varargs int use_skill(string skill, mixed mod_adjust);
 int assure_skill(string skill);
+private string determine_skill_to_improve(string skill, float skill_cap);
+private float clamp_improvement(string skill_name, float improvement);
 
 #endif // __SKILLS_H__
