71fe614d3512f5e9a7d0703e7aa8dbbb7e6f5a7b
gesslar <1266935+gesslar@users.noreply.github.com>
2026-09-10 13:27:14 -0400

    adding health bars

--- std/living/include/body.h
+++ std/living/include/body.h
@@ -11,6 +11,10 @@ public string *query_weapon_slots();
 public void set_su_body(object source);
 public object query_su_body();
 public void clear_su_body();
-public void display_health_bar();
+protected void display_health_bar();
+private void display_plain_health_bar();
+private void display_auto_health_bar();
+private int *get_vital_ratio(float curr, float max, int constraint);
+protected varargs void display_enemy_health_bar(object who);
 
 #endif // __BODY_H__
