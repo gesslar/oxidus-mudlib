63a91da7cb1436809f44e69c86b0a899b8717a7b
gesslar <1266935+gesslar@users.noreply.github.com>
2026-09-16 02:32:43 -0400

    skills update

--- std/living/include/skills.h
+++ std/living/include/skills.h
@@ -1,28 +1,28 @@
 #ifndef __SKILLS_H__
 #define __SKILLS_H__
 
-void wipe_skills();
-varargs void initialize_missing_skills(mapping skill_set, string curr_path);
-varargs int add_skill(string skill, float level);
-int remove_skill(string skill);
-float query_raw_skill(string skill);
-float query_skill(string skill);
-float query_skill_level(string skill);
-float query_raw_skill_level(string skill);
-int has_skill(string skill);
-int set_skill_level(string skill, float level);
-mapping query_skills();
-void set_skills(mapping s);
-varargs float improve_skill(string skill, mixed potential_progress);
-int query_skill_progress(string skill);
-int modify_skill_level(string skill, int level);
-public int adjust_skills_by_npc_level(float level);
+public void wipe_skills();
+public varargs void initialize_missing_skills(mapping skill_set, string curr_path);
+public varargs int add_skill(string skill, float level);
+public int remove_skill(string skill);
 private nomask mapping find_skill_node(string skill);
-private nomask mapping adjust_skill_levels(mapping current_skills, float level);
-string *query_skill_path(string skill);
-public varargs int use_skill(string skill, mixed mod_adjust);
-int assure_skill(string skill);
-private string determine_skill_to_improve(string skill, float skill_cap);
+public float query_raw_skill(string skill);
+public float query_skill(string skill);
+public float query_skill_level(string skill);
+public float query_raw_skill_level(string skill);
+public int has_skill(string skill);
+public int set_skill_level(string skill, float level);
+public mapping query_skills();
+public void set_skills(mapping s);
+public varargs int use_skill(string skill, mixed improvement, mixed improvement_chance);
 private float clamp_improvement(string skill_name, float improvement);
+public string *query_skill_path(string skill);
+private string determine_skill_to_improve(string skill_name, float skill_cap);
+public varargs float improve_skill(string skill_name, mixed potential_progress);
+public int query_skill_progress(string skill);
+public int modify_skill_level(string skill, int level);
+public int adjust_skills_by_npc_level(float level);
+private nomask mapping adjust_skill_levels(mapping current_skills, float level);
+public int assure_skill(string skill);
 
 #endif // __SKILLS_H__
